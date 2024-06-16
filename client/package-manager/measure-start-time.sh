fe8a490adc2932158a1dad8ac7e137202d9cdaf1
Victor Homyakov <vkhomyackov@gmail.com>
2024-06-16 16:47:57 +0400

    fix: Update package manager benchmarks

--- client/package-manager/measure-start-time.sh
+++ client/package-manager/measure-start-time.sh
@@ -1,9 +1,13 @@
+node -v && bun -v
+
 hyperfine --warmup 3 \
   'bun modules-bun/index.js' \
   'bun modules-npm/index.js' \
   'bun modules-pnpm/index.js' \
   'bun modules-yarn/index.js' \
+  'bun modules-yarn4/index.js' \
   'node modules-bun/index.js' \
   'node modules-npm/index.js' \
   'node modules-pnpm/index.js' \
-  'node modules-yarn/index.js'
+  'node modules-yarn/index.js' \
+  'node modules-yarn4/index.js'
