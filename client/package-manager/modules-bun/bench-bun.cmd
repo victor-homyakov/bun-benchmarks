9f2a9f9f91e3f3b9cdaeacbb9983b71bf9688280
Victor Homyakov <vkhomyackov@gmail.com>
2024-08-04 15:54:25 +0400

    fix: Add comments to benchmark scripts

--- client/package-manager/modules-bun/bench-bun.cmd
+++ client/package-manager/modules-bun/bench-bun.cmd
@@ -1,6 +1,10 @@
 rem Install ptime 1.0 for Win32 from http://www.pc-tools.net/
 bun -v
+rem холодная первая установка
 bun pm cache rm && ptime bun install
+rem горячая первая установка
 bun pm cache rm && rm -rf node_modules && ptime bun install
+rem переустановка
 rm -rf node_modules && ptime bun install
+rem валидация
 ptime bun install
